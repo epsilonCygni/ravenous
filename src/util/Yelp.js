82e521995e7c543df7ce1a2770b992c0b004a41e
Cygnus <butuc_sammy@yahoo.com>
2019-04-16 17:57:56 +0300

    Build Front-end Application with React -- Ravenous

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+const apiKey = 'vuoNSYa8nLq_bO4spUOoJMkHEPQBVwwlQrFwT_Euo6DFM9HRcGokNwBTSBuDP3BSl4lVd9pfpsFVLoxzqtSWT3QyEcxMCJ8JfFERjGaa9kwE_7neo42MA7qhowWyXHYx';
+
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    console.log(business);
+                    
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.postal_code,
+                        category: business.categories.title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                });
+            }
+        });
+    }
+};
+
+export default Yelp;
