461c86ef28f967a7350491d2ddd138007076eece
Cygnus <butuc_sammy@yahoo.com>
2019-04-16 17:44:44 +0300

    Updates

---
+++ precache-manifest.703da522d9b5edeb41d722e68e95c49f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "8de96ce6836fcb67b61c",
+    "url": "/ravenous/static/js/main.d8ccc1ad.chunk.js"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/ravenous/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "8de96ce6836fcb67b61c",
+    "url": "/ravenous/static/css/main.66a3dd77.chunk.css"
+  },
+  {
+    "revision": "8916d6f837ed6652f7d45df1501539c8",
+    "url": "/ravenous/index.html"
+  }
+];
